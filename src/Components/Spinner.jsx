f482ef55725946eff769dc1d890b279fe29206a0
Rudresh oza <62214751+rudreshoza18@users.noreply.github.com>
2021-10-14 03:09:31 +0530

    Update Spinner.jsx

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -3,11 +3,13 @@ import React   from 'react'
  const Spinner =(props)=> {
     
         return (
+         <>
             <div className="ui active dimmer">
                 <div className="ui massive text loader">
                     {props.message ||"Loading..." }
                 </div>
             </div>
+</>
         )
     
 }
